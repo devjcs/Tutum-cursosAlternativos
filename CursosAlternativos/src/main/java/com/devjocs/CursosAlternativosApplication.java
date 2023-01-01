95ad3273d0776039977c95f7fccf858589587b9f
devjocs <devjocs@gmail.com>
2023-01-01 16:54:20 -0600

    init

---
+++ CursosAlternativos/src/main/java/com/devjocs/CursosAlternativosApplication.java
@@ -0,0 +1,13 @@
+package com.devjocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosAlternativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosAlternativosApplication.class, args);
+	}
+
+}
---
+++ CursosAlternativos/src/test/java/com/devjocs/CursosAlternativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosAlternativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
